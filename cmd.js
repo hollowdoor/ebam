c48ca712c5006df4f0f0459f256ad8b985e6ddac
Quentin Engles <hollowdoor99@gmail.com>
2018-04-19 15:06:54 -0500

    Added to cli help

--- cmd.js
+++ cmd.js
@@ -11,6 +11,11 @@ const cliInit = require('./lib/cli_init.js');
     run.cliInit().then(cmd=>{
         const cli = cmd.cli;
 
+        if(cli.flags.help){
+            console.log(cli.help);
+            return;
+        }
+
         if(cli.input[0] === 'init'){
             init();
             return;
